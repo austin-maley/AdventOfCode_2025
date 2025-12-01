fc4a0ad9ceb2c1ba4ba7111d9b3ff6c450534f75
SDI\austin.maley <austin.maley@steeldynamics.com>
2025-12-01 15:21:35 -0500

    remove unused

--- AdventOfCode_2025/Day1/Day1Part1.cs
+++ AdventOfCode_2025/Day1/Day1Part1.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text.RegularExpressions;
-
-namespace AdventOfCode_2025.Day1
+﻿namespace AdventOfCode_2025.Day1
 {
 	public class Day1Part1(int startingValue)
 	{
